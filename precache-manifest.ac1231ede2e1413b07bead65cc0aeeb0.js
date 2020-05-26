0608181f4f973394809e8137afd7643f714e0a79
Kathleen Tonner <kathleentonner@gmail.com>
2020-05-26 11:57:10 -0400

    Updates

---
+++ precache-manifest.ac1231ede2e1413b07bead65cc0aeeb0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60a7abcdf12b66e1b5a246ca8a853c5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c5957cf7bbccf8abff9b",
+    "url": "/Portfolio/static/css/main.f40b09fd.chunk.css"
+  },
+  {
+    "revision": "d559c64ac59b563c0f7d",
+    "url": "/Portfolio/static/js/2.5ec599d4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.5ec599d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5957cf7bbccf8abff9b",
+    "url": "/Portfolio/static/js/main.8f95906b.chunk.js"
+  },
+  {
+    "revision": "4a50002865ce6bd649aa",
+    "url": "/Portfolio/static/js/runtime-main.d26897fe.js"
+  },
+  {
+    "revision": "f302f47c1d63007361072b76efc305a9",
+    "url": "/Portfolio/static/media/booklogger.f302f47c.png"
+  },
+  {
+    "revision": "8173b035d2d738093a464b7c0c19248e",
+    "url": "/Portfolio/static/media/budget.8173b035.jpg"
+  },
+  {
+    "revision": "4868aa1468707b20a55458e3e91ffe38",
+    "url": "/Portfolio/static/media/codingquiz.4868aa14.png"
+  },
+  {
+    "revision": "d28c17a9504fbee3fc0b1d9dee7a3609",
+    "url": "/Portfolio/static/media/itme.d28c17a9.jpg"
+  },
+  {
+    "revision": "6786b91aafbf39b46024b58d298c7e58",
+    "url": "/Portfolio/static/media/landscape.6786b91a.jpg"
+  },
+  {
+    "revision": "29e835d562ac47997a2ac8ef467d8829",
+    "url": "/Portfolio/static/media/notetaker.29e835d5.png"
+  },
+  {
+    "revision": "a6310c3977f132604c57609e89186a2b",
+    "url": "/Portfolio/static/media/rose-quartz.a6310c39.jpg"
+  },
+  {
+    "revision": "8c315cc50148f96be2114a0d54063281",
+    "url": "/Portfolio/static/media/weatherdash.8c315cc5.png"
+  }
+]);
