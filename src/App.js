c426c83eca55bddea4f62607f62258f2740f0af7
unknown <kathleentonner@gmail.com>
2020-10-08 13:35:35 -0400

    trying to fix gh-pages again

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ const history = createHistory();
 function App() {
   return (
     <div className="App">
-      <Router history={history}>
+      <Router history={history} basename={process.env.PUBLIC_URL}>
         <Navbar />
         <HomePage/>
         <Route path="/" exact component={HomePage} />
+        <Route path="/" exact component={Work} />
       </Router>
     </div>
   );
